520f2edf0dc694b8b541a0314c83c69883f7f9ca
Ivan Mitkov <ivan.mitkov@gmail.com>
2021-09-15 15:09:25 +0300

    Supplying context with decorators - mock redux store

--- taskbox/src/components/InboxScreen.stories.js
+++ taskbox/src/components/InboxScreen.stories.js
@@ -1,11 +1,25 @@
 import React from "react";
-
+import { Provider } from "react-redux";
+import { action } from "@storybook/addon-actions";
+import * as TaskListStories from "./TaskList.stories";
 import { PureInboxScreen } from "./InboxScreen";
+
+// A super-simple mock of a redux store
+const store = {
+  getState: () => {
+    return {
+      tasks: TaskListStories.Default.args.tasks,
+    };
+  },
+  subscribe: () => 0,
+  dispatch: action("dispatch"),
+};
 // for the PureInboxScreen we have a problem because although the PureInboxScreen itself is presentational, its child, the TaskList, is not. In a sense the PureInboxScreen has been polluted by “container-ness”. So when we setup our stories in InboxScreen.stories.js:
 //One way to sidestep this problem is to never render container components anywhere in your app except at the highest level and instead pass all data-requirements down the component hierarchy.
 export default {
   component: PureInboxScreen,
   title: "InboxScreen",
+  decorators: [(story) => <Provider store={store}>{story()}</Provider>],
 };
 
 const Template = (args) => <PureInboxScreen {...args} />;
